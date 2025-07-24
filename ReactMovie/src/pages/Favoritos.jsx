793d72618e3921dc1048e7dee2212f5e1443759e
Ignacio Aprile <ignacioaprile@hotmail.com>
2025-07-24 18:37:54 -0300

    correción para deploy en favoritos

--- ReactMovie/src/pages/Favoritos.jsx
+++ ReactMovie/src/pages/Favoritos.jsx
@@ -8,24 +8,24 @@ function Favoritos(){
     
     return(
         <section className="main3">
-            {favoritosPeli.length > 0 &&
+            {favoritosPeli?.length > 0 &&
             <article>
                 <h2 className="titulo2">Mis peliculas Favoritas</h2>
                 <ul className="grid-cards">
-                {favoritosPeli.map((peli)=>(
+                {favoritosPeli.length > 0 ? favoritosPeli.map((peli)=>(
                     <MovieCard key={peli.id} pelicula={peli} />
-                ))}
+                )): <p>❌No hay peliculas favoritas❌</p>}
                 </ul> 
             </article>
             }
               
-            {favoritosTv.length > 0 && 
+            {favoritosTv?.length > 0 && 
             <article className="main3Videos">
                 <h2 className="titulo2">Mis Tv Favoritas</h2>
                 <ul className="grid-cards">
-                    {favoritosTv.map((tv)=>(
+                    {favoritosTv.length > 0 ? favoritosTv.map((tv)=>(
                         <TvCard tvShow={tv} key={tv.id}/>
-                    ))}
+                    )):<p>❌No hay favoritos Tv❌</p>}
                 </ul>
             </article>
             }
